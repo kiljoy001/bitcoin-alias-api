337f03504d84a7177de5651659e2b266aa604b3f
Scott Guyton <kiljoy001@gmail.com>
2020-01-27 01:26:11 -0800

    get appears to be working, (search gives 0 results) Need to test transfer / create transactions
    Signed-off-by: Scott Guyton <kiljoy001@gmail.com>

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
+import sys
 from flask import Flask
 from flask_restful import Api, Resource, reqparse
 from bigchaindb_driver import BigchainDB
-from .settings import AppSettings
-from Asset_Handling.create_user import User
-from Asset_Handling.asset_template import Asset
+from settings import AppSettings
+
+sys.path.append('./Asset_Handling/')
 from Asset_Handling.exceptions import InvalidAliasException
 
 app = Flask(__name__)
@@ -12,23 +13,25 @@ settings = AppSettings.get_settings()
 
 
 class Query(Resource):
+    BDB = BigchainDB(settings['bigchainurl'])
 
-    def __init__(self):
-        self.bdb = BigchainDB(settings['bigchainurl'])
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('alias')
+        params = parser.parse_args()
 
-    def get(self, alias):
-        if len(self.bdb.metadata.get(alias)) > 0:
-            return self.bdb.metadata.get(alias), 200
+        if len(Query.BDB.metadata.get(search=params['alias'])) > 0:
+            return {'alias': Query.BDB.metadata.get(params['alias'])}, 200
         else:
-            return "No results", 404
+            return {'result': "No results found"}, 404
 
     def post(self):
-        # TODO: Need to change this part to accept a signed transaction from the client. This setup is wrong / dangerous
+        # TODO: create a test payload and use postman to test if it accepts signed transactions
         try:
             parser = reqparse.RequestParser()
             parser.add_argument('signed_transaction')
             params = parser.parse_args()
-            transaction_id = self.bdb.transactions.send_commit(params['signed_transaction'])
+            transaction_id = Query.BDB.transactions.send_commit(params['signed_transaction'])
             json_return = {
                 'txid': transaction_id['metadata'],
             }
@@ -41,13 +44,21 @@ class Query(Resource):
         except InvalidAliasException:
             return "Alias already exists!", 400
 
-    @property
+    # Todo: this method can be removed, since signed transactions are being accepted, no need to make a special endpoint
     def update(self):
         try:
             parser = reqparse.RequestParser()
             parser.add_argument('signed_transaction')
             params = parser.parse_args()
-            self.bdb.transactions.send_commit(params['signed_transaction'])
+            Query.BDB.transactions.send_commit(params['signed_transaction'])
 
         except:
             return "Transaction error!", 400
+
+#Todo: Warning: Silently ignoring app.run() because the application is run from the flask command line executable.  \
+# Consider putting app.run() behind an if __name__ == "__main__" guard to silence this warning.
+api.add_resource(Query, '/')
+if settings['debug_on'] == 'True':
+    app.run(debug=True)
+else:
+    app.run(debug=False)
