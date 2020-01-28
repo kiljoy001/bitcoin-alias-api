e502c6ba33386a7f362231d10c12fe645a1a586f
Scott Guyton <kiljoy001@gmail.com>
2020-01-28 02:55:27 -0800

    Added TODO's, api test skelly, adding jwt to get endpoint
    Signed-off-by: Scott Guyton <kiljoy001@gmail.com>

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask_restful import Api, Resource, reqparse
 from bigchaindb_driver import BigchainDB
 from settings import AppSettings
+from flask_jwt import JWT, jwt_required, current_identity
 
 sys.path.append('./Asset_Handling/')
 from Asset_Handling.exceptions import InvalidAliasException
@@ -10,11 +11,14 @@ from Asset_Handling.exceptions import InvalidAliasException
 app = Flask(__name__)
 api = Api(app)
 settings = AppSettings.get_settings()
+app.secret_key = settings['secret_key']
 
 
 class Query(Resource):
     BDB = BigchainDB(settings['bigchainurl'])
 
+    # TODO: add jwt auth to this
+    @jwt_required()
     def get(self):
         parser = reqparse.RequestParser()
         parser.add_argument('alias')
@@ -32,38 +36,19 @@ class Query(Resource):
             parser.add_argument('signed_transaction')
             params = parser.parse_args()
             transaction_id = Query.BDB.transactions.send_commit(params['signed_transaction'])
-            json_return = {
-                'txid': transaction_id['metadata'],
-            }
-
-            return json_return, 201
+            return {'transaction_id': transaction_id['metadata']}, 201
 
         except ValueError:
-            # Todo: change to json
-            return "Value error", 400
+            return {'result': "Value error"}, 400
 
         except InvalidAliasException:
-            # Todo: change to json
-            return "Alias already exists!", 400
-
-    # Todo: this method can be removed, since signed transactions are being accepted, no need to make a special endpoint
-    def update(self):
-        try:
-            parser = reqparse.RequestParser()
-            parser.add_argument('signed_transaction')
-            params = parser.parse_args()
-            Query.BDB.transactions.send_commit(params['signed_transaction'])
-
-        except:
-            return "Transaction error!", 400
+            return {'result': "Alias already exists!"}, 400
 
 
-# Todo: Warning: Silently ignoring app.run() because the application is run from the flask command line executable.  \
-#  Consider putting app.run() behind an if __name__ == "__main__" guard to silence this warning.
-
-# TODO: Change endpoint from root to /Query
-api.add_resource(Query, '/')
+api.add_resource(Query, '/Query')
 if settings['debug_on'] == 'True':
-    app.run(debug=True)
+    if __name__ == "__main__":
+        app.run(debug=True)
 else:
-    app.run(debug=False)
+    if __name__ == "__main__":
+        app.run(debug=False)
---
+++ tests_api.py
@@ -0,0 +1,12 @@
+from flask_testing import TestCase
+
+
+class GetDataTests(TestCase):
+
+    def create_app(self):
+        pass
+
+
+class PostDataTests(TestCase):
+    def create_app(self):
+        pass
